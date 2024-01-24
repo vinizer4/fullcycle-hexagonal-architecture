92a56d16081859ab8ac949df08fb10f2aa8dcb91
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2024-01-24 20:46:37 -0300

    initial setup and defined product interface

---
+++ application/product.go
@@ -0,0 +1,53 @@
+package application
+
+import "errors"
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+//func (p *Product) IsValid() (bool, error) {
+//
+//}
+
+func (p *Product) Enable() error {
+
+}
+
+//func (p *Product) Disable() error {
+//
+//}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
