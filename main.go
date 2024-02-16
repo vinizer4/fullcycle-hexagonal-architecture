1c4416be0835e4f64e2f3011cc59b00760c9ce42
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2024-02-15 23:02:39 -0300

    feat: created cli adapter and the unit test

---
+++ main.go
@@ -0,0 +1,17 @@
+package fullcycle_hexagonal_architecture
+
+import (
+	"database/sql"
+	db2 "github.com/codeedu/go-hexagonal/adapters/db"
+	"github.com/codeedu/go-hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, _ := sql.Open("sqlite3", ":db.sqlite")
+	productDbAdapter := db2.NewProductDb(db)
+	productService := application.NewProductService(productDbAdapter)
+	product, _ := productService.Create("Product Exemplo", 30)
+
+	productService.Enable(product)
+}
